525be59a11c894a221c476d7157c4a9c05998a0c
Risa Goto <omusubi5656dup@gmail.com>
2025-01-24 17:57:29 +0900

    add:コメント

--- counter.js
+++ counter.js
@@ -1,17 +1,32 @@
+// 即時実行関数で全体を囲み、グローバルスコープを汚さないようにしています
 (() => {
-    const $counter = document.getElementById("js-counter");
+  // カウンターの値を表示する要素を取得
+  const $counter = document.getElementById("js-counter");
 
-    const clickHandler = (e) => {
-        const $targetButton = e.currentTarget;
-        let currentCount = parseInt($counter.textContent);
-        if($targetButton.textContent === "+"){
-            $counter.textContent = currentCount + 1;
-        } else {
-            $counter.textContent = currentCount - 1;
-        }
-    }
+  // ボタンがクリックされた時の処理を定義
+  const clickHandler = (e) => {
+    // クリックされたボタン要素を取得
+    const $targetButton = e.currentTarget;
+    // 現在のカウンター値を数値として取得
+    let currentCount = parseInt($counter.textContent);
 
-    for (let index = 0; index < document.getElementsByClassName("js-button").length; index++) {
-        document.getElementsByClassName("js-button")[index].addEventListener("click", (e) => clickHandler(e))
+    // プラスボタンがクリックされた場合は加算、マイナスボタンの場合は減算
+    if ($targetButton.textContent === "+") {
+      $counter.textContent = currentCount + 1;
+    } else {
+      $counter.textContent = currentCount - 1;
     }
+  };
+
+  // ページ内の全ての「js-button」クラスを持つ要素に対して
+  // クリックイベントリスナーを設定
+  for (
+    let index = 0;
+    index < document.getElementsByClassName("js-button").length;
+    index++
+  ) {
+    document
+      .getElementsByClassName("js-button")
+      [index].addEventListener("click", (e) => clickHandler(e));
+  }
 })();
